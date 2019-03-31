50a9d461f600ff3b155774c30a8eb733df72249d
Tyshchenko Andrey <ggoldenfeniks@gmail.com>
2019-03-31 23:11:48 +1000

    Add better computation with multiple modes

--- include/solver.hpp
+++ include/solver.hpp
@@ -36,86 +36,59 @@ namespace acstc {
             _na = _b1 / den;
         }
 
-        template<typename IV, typename KV, typename PV, typename FA>
-        void operator()(const IV& init, const KV& ks, const PV& phi, FA&& callback_factory, const size_t k = 10, const size_t past_n = 0) const {
-            types::vector1d_t<Val> nv(_ny), cv(_ny), ov(_ny);
-            Val s0;
-            types::vector1d_t<Val> va(_ny, _na), vb(_ny), cb(_ny), fv(_nx), lv(_nx), ss(_nx);
+        template<typename IN, typename KJ, typename PJ, typename CL>
+        void operator()(const IN& init, const KJ& k, const PJ& phi, CL&& callback, const size_t past_n = 0) const {
+            const auto mc = k.size();
+            if (init.size() != mc || phi.size() != mc)
+                throw std::logic_error("Vectors init, k, phi must be the same size");
+            types::vector1d_t<Val> nv(_ny), ov(_ny, Val(0)), va(_ny, _na);
+            types::vector2d_t<Val> cv(mc, types::vector1d_t<Val>(_ny)),
+                                   cb(mc, types::vector1d_t<Val>(_ny)),
+                                   vb(mc, types::vector1d_t<Val>(_ny)),
+                                   fs(mc, types::vector1d_t<Val>(_nx)),
+                                   ls(mc, types::vector1d_t<Val>(_nx)),
+                                   fv(mc, types::vector1d_t<Val>(_nx)),
+                                   lv(mc, types::vector1d_t<Val>(_nx));
+            const auto s0 = im * tw * _c / _k0 / (_b - _c) / _hx - on;
+            va[0] = va.back() = s0 + tw;
 
-            for (const auto& pj : phi) {
-                auto callback = callback_factory();
-                for (const auto& [vv, kj] : utils::zip(init, ks)) {
-                    const auto nb = std::pow(kj[0] / _k0, 2);
-
-                    for (size_t m = 0; m < _ny; ++m) {
-                        const auto dd = (std::pow(kj[m], 2) - _sq_k0 - tw / _sq_hy) / _sq_k0;
-                        cb[m] = _g0 + _g1 * dd;
-                        vb[m] = _b0 + _b1 * dd;
-                    }
-
-                    const auto de = on - _c * (on - nb);
-                    const auto rr = tw * _k0 * _sq_hy / (_b - _c) / _hx;
-                    const auto qq = tw * _c / _k0 / (_b - _c) / _hx;
-                    const auto ka = _hx * _k0 * (_a - on - (_b - _c) * (on - nb)) / tw;
-                    const auto ga = rr * de;
-                    const auto si = ka * -rr;
-                    const auto pg = ga + im * si;
-                    const auto ng = ga - im * si;
-                    const auto gq = ga - Arg(4) * qq;
-                    const auto s4 = si + Arg(4);
-                    const auto aa = pg * (gq + im * s4);
-                    const auto bb = ga * gq + si * s4;
-                    const auto cc = ng * (gq - im * s4);
-                    const auto sa = _psqrt(aa);
-                    const auto sc = _psqrt(cc);
-                    const auto la = sa / sc;
-                    const auto mu = bb / sa / sc;
-                    const auto pq = on + im * qq;
-                    const auto nq = on - im * qq;
-                    const auto xi = std::arg(nq / pq);
-                    const auto ph = std::arg(aa);
-                    const auto bh = pg + ng * std::exp(-im * xi);
-                    const auto ze = im * std::sqrt(std::abs(aa)) * std::exp(im * ph / tw) / tw;
-                    const auto be = nq + im * ng / tw + cc * (pq - im * pg / tw) / bb;
-
-                    ss[0] = pq - im * (pg + sa) / tw;
-                    ss[1] = nq + im * (ng + bb / sa) / tw;
-                    ss[2] = mu * (ss[1] + ss[0] / la / mu - be) / la / tw;
-                    for (size_t n = 2; n < _nx - 1; ++n)
-                        ss[n + 1] = (Arg(2 * n - 1) * mu * ss[n] / la -
-                                        Arg(n - 2) * ss[n - 1] / std::pow(la, 2)) / Arg(n + 1);
-                    va[0] = va.back() = pq;
-                    vb[0] = vb.back() = -ss[0];
-                    s0 = -nq;
-
-                    cv = vv;
-                    for (size_t i = 0; i < _ny; ++i) {
-                        ov[i] = cv[i] * pj[i];
-                    }
-                    callback(ov);
-
-                    for (size_t n = 1; n < _nx; ++n) {
-                        nv[0] = s0 * cv[1];
-                        nv.back() = s0 * cv[_ny - 2];
-                        for (size_t m = _start_index(n, past_n); m < n; ++m) {
-                            nv[0] += fv[m] * ss[n - m];
-                            nv.back() += lv[m] * ss[n - m];
-                        }
-
-                        for (size_t m = 1; m < _ny - 1; ++m)
-                            nv[m] = (cv[m - 1] + cv[m + 1]) * _ca + cv[m] * cb[m];
-
-                        _thomas_solver(va, vb, nv);
-                        std::swap(cv, nv);
-
-                        fv[n] = cv[0];
-                        lv[n] = cv.back();
-
-                        for (size_t i = 0; i < _ny; ++i)
-                            ov[i] = cv[i] * pj[i];
-                        callback(ov);
-                    }
+            for (size_t j = 0; j < mc; ++j) {
+                for (size_t i = 0; i < _ny; ++i) {
+                    const auto dd = (std::pow(k[j][i], 2) - _sq_k0 - tw / _sq_hy) / _sq_k0;
+                    cb[j][i] = _g0 + _g1 * dd;
+                    vb[j][i] = _b0 + _b1 * dd;
+                    cv[j][i] = init[j][i];
+                    ov[i] += phi[j][i] * init[j][i];
                 }
+                _fill_parameters(k[j][0], fs[j], vb[j][0]);
+                _fill_parameters(k[j].back(), ls[j], vb[j].back());
+            }
+
+            callback(ov);
+
+            for (size_t i = 1; i < _nx; ++i) {
+                ov.assign(_ny, Val(0));
+                for (size_t j = 0; j < mc; ++j) {
+                    nv[0] = s0 * cv[j][1];
+                    nv.back() = s0 * cv[j][_ny - 2];
+                    for (size_t m = _start_index(i, past_n); m < i; ++m) {
+                        nv[0] += fv[j][m] * fs[j][i - m];
+                        nv.back() += lv[j][m] * ls[j][i - m];
+                    }
+
+                    for (size_t m = 1; m < _ny - 1; ++m)
+                        nv[m] = (cv[j][m - 1] + cv[j][m + 1]) * _ca + cv[j][m] * cb[j][m];
+
+                    _thomas_solver(va, vb[j], nv);
+                    std::swap(cv[j], nv);
+
+                    fv[j][i] = cv[j][0];
+                    lv[j][i] = cv[j].back();
+
+                    for (size_t m = 0; m < _ny; ++m)
+                        ov[m] += phi[j][m] * cv[j][m];
+                }
+                callback(ov);
             }
         }
 
@@ -139,6 +112,42 @@ namespace acstc {
         static auto _psqrt(const Val& value) {
             const auto res = std::sqrt(value);
             return res.real() < 0 ? -res : res;
+        }
+
+        void _fill_parameters(const Val& k, types::vector1d_t<Val>& ss, Val& vb) const {
+            const auto nb = std::pow(k / _k0, 2);
+            const auto de = on - _c * (on - nb);
+            const auto rr = tw * _k0 * _sq_hy / (_b - _c) / _hx;
+            const auto qq = tw * _c / _k0 / (_b - _c) / _hx;
+            const auto ka = _hx * _k0 * (_a - on - (_b - _c) * (on - nb)) / tw;
+            const auto ga = rr * de;
+            const auto si = ka * -rr;
+            const auto pg = ga + im * si;
+            const auto ng = ga - im * si;
+            const auto gq = ga - Arg(4) * qq;
+            const auto s4 = si + Arg(4);
+            const auto aa = pg * (gq + im * s4);
+            const auto bb = ga * gq + si * s4;
+            const auto cc = ng * (gq - im * s4);
+            const auto sa = _psqrt(aa);
+            const auto sc = _psqrt(cc);
+            const auto la = sa / sc;
+            const auto mu = bb / sa / sc;
+            const auto pq = on + im * qq;
+            const auto nq = on - im * qq;
+            const auto xi = std::arg(nq / pq);
+            const auto ph = std::arg(aa);
+            const auto bh = pg + ng * std::exp(-im * xi);
+            const auto ze = im * std::sqrt(std::abs(aa)) * std::exp(im * ph / tw) / tw;
+            const auto be = nq + im * ng / tw + cc * (pq - im * pg / tw) / bb;
+
+            ss[0] = pq - im * (pg + sa) / tw;
+            ss[1] = nq + im * (ng + bb / sa) / tw;
+            ss[2] = mu * (ss[1] + ss[0] / la / mu - be) / la / tw;
+            for (size_t n = 2; n < _nx - 1; ++n)
+                ss[n + 1] = (Arg(2 * n - 1) * mu * ss[n] / la -
+                                Arg(n - 2) * ss[n - 1] / std::pow(la, 2)) / Arg(n + 1);
+            vb = -ss[0];
         }
 
         auto _thomas_solver(const types::vector1d_t<Val>& a,
